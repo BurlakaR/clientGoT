d2554a6eb55bc76fed6340f2a1a99530427ea591
BurlakaR <bugaga1998@gmail.com>
2018-11-05 10:03:00 +0200

    Ilon Mask

--- src/main/java/com/client/Starter.java
+++ src/main/java/com/client/Starter.java
@@ -1,13 +1,30 @@
 package com.client;
 
 
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Starter {
-    public static void main(String[] args) {
-        SpringApplication.run(Starter.class, args);
+public class Starter extends AbstractJavaFxApplicationSupport {
+
+    @Autowired
+    private ConfigurationControllers.View view;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Fuck this shit");
+        stage.setScene(new Scene(view.getView()));
+        stage.setResizable(true);
+        stage.centerOnScreen();
+        stage.show();
     }
-    //just checking the branch
+
+    public static void main(String[] args) {
+        launchApp(Starter.class, args);
+    }
+
 }
